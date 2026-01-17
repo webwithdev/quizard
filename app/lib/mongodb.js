31b83696a993f123d6fae069e18903cd54aba18e
webwithdev <bishtdevansh03@gmail.com>
2026-01-17 18:14:01 +0530

    add env

--- app/lib/mongodb.js
+++ app/lib/mongodb.js
@@ -1,8 +1,11 @@
 import { MongoClient } from "mongodb";
 
-const url="mongodb+srv://bishtdevansh03_db_user:0AIMXuNcFWpgN3Iq@devansh.kjmo6as.mongodb.net/"
+const uri = process.env.MONGODB_URI;
+let client = new MongoClient(uri)
 
-let client = new MongoClient(url)
+if (!uri) {
+  throw new Error("❌ MONGODB_URI is not defined");
+}
 
 const options = {};
 
@@ -11,13 +14,13 @@ let clientPromise;
 
 if (process.env.NODE_ENV === "development") {
   if (!global._mongoClientPromise) {
-    client = new MongoClient(url, options);
+    client = new MongoClient(uri, options);
     global._mongoClientPromise = client.connect();
   }
   clientPromise = global._mongoClientPromise;
-} else {   
-  
-  client = new MongoClient(url, options);
+} else {
+
+  client = new MongoClient(uri, options);
   clientPromise = client.connect();
 }
 
