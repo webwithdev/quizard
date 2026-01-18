72c5eede12bc02fcae42fd0302cc1da454eed902
webwithdev <bishtdevansh03@gmail.com>
2026-01-18 19:21:06 +0530

    Updated code

--- app/quiz/page.js
+++ app/quiz/page.js
@@ -68,10 +68,10 @@ const page = () => {
 
   const categories = ["General Knowledge", "Entertainment: Books", "Entertainment: Film", "Entertainment: Music", "Entertainment: Musicals & Theatres", "Entertainment: Television", "Entertainment: Video Games", "Entertainment: Board Games", "Science & Nature", "Science: Computers", "Science: Mathematics", "Mythology", "Sports", "Geography", "History", "Politics", "Art", "Celebrities", "Animals", "Vehicles", "Entertainment: Comics", "Science: Gadgets", "Entertainment: Japanese Anime & Manga", "Entertainment: Cartoon & Animations"]
   
- let categorynum
+
  for (let i = 0; i < categories.length; i++) {
   if(selectcategory==categories[i]){
-    categorynum=i+9
+    setCategorynumber(i+9)
     
   }
  }
