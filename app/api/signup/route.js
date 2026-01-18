829052a61f44c35a1e0ea4ff980001f021c25f18
webwithdev <bishtdevansh03@gmail.com>
2026-01-18 18:53:59 +0530

    Updated code

--- app/api/signup/route.js
+++ app/api/signup/route.js
@@ -50,7 +50,7 @@ export async function POST(request) {
 
     // ✅ 5. INSERT USER
     await db.collection("users").insertOne({
-      username,
+      name,
       email,
       password: hashedPassword,
       createdAt: new Date()
